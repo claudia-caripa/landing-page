570f67f7585c5b5fa9541c90dc4e9bde6eceb51c
Claudia Sanchez <claudia.caripa@gmail.com>
2020-06-02 10:45:46 +0100

    Build navigation menu, add distinguish section view, add scroll to sections

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,12 +18,16 @@
  * 
 */
 
+const sectionNames = document.querySelectorAll('section');
+const navList = document.getElementById('navbar__list');
+
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
 
 
 
@@ -34,13 +38,61 @@
 */
 
 // build the nav
+function generateMenu() {
+    const fragment = document.createDocumentFragment();
+    
+    sectionNames.forEach(function(name){
+        let text = name.dataset.nav;
+        let id = name.id;
+        let li = document.createElement('li');
+        li.innerHTML = '<a href="#' + id + '" class="menu__link">' + text + '</a>';
+        fragment.appendChild(li);
+        
+    });
+
+    navList.appendChild(fragment);
+}
 
 
 // Add class 'active' to section when near top of viewport
+function highlightSection() {
+    sectionNames.forEach(function(name){
+        let bounding = name.getBoundingClientRect();
+        if (bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        ){
+            name.className = "your-active-class";
+        } else {
+            name.className = "";
+        }
+
+    });
+}
 
 
-// Scroll to anchor ID using scrollTO event
 
+// Scroll to anchor ID using scrollTop event
+function smoothScroll(event){
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+          // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if   
+};
 
 /**
  * End Main Functions
@@ -49,9 +101,12 @@
 */
 
 // Build menu 
+generateMenu();
 
 // Scroll to section on link click
+$("a").on('click', smoothScroll);
 
 // Set sections as active
+window.onscroll = function() {highlightSection()};
 
 
